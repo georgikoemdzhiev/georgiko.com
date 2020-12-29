1fd0537803e6c4f2fa0bcb1548685dd943a801d2
georgikoemdzhiev <koemdjiev@gmail.com>
2020-12-29 15:13:20 +0000

    Update to Angular 11

--- src/app/post-detail/post-detail.component.spec.ts
+++ src/app/post-detail/post-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PostDetailComponent } from './post-detail.component';
 
@@ -6,7 +6,7 @@ describe('PostDetailComponent', () => {
   let component: PostDetailComponent;
   let fixture: ComponentFixture<PostDetailComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PostDetailComponent ]
     })
